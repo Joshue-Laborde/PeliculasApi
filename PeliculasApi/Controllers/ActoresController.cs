e90e1c121477b35e2794f881f67a9a6961739058
Joshue Laborde <joshuelab@gmail.com>
2023-05-19 16:48:53 -0500

    Creación de módulo de Actores

---
+++ PeliculasApi/Controllers/ActoresController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasApi.DTOs;
+using PeliculasApi.Entidades;
+
+namespace PeliculasApi.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ActoresController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActorDTO>>> Get()
+        {
+            var actores = await context.Actores.ToListAsync();
+            return mapper.Map<List<ActorDTO>>(actores);
+        }
+
+        [HttpGet("{id}", Name ="obtenerActor")]
+        public async Task<ActionResult<ActorDTO>> GetId(int id)
+        {
+            var actor = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+            if(actor == null)
+                return NotFound();
+            return mapper.Map<ActorDTO>(actor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] ActorCreacionDTO actorCreacionDTO)
+        {
+            var actor = mapper.Map<Actor>(actorCreacionDTO);
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            var result = mapper.Map<ActorDTO>(actor);
+
+            return new CreatedAtRouteResult("obtenerActor", new { id = actor.Id }, result);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromForm] ActorCreacionDTO actorCreacionDTO)
+        {
+            var actor = mapper.Map<Actor>(actorCreacionDTO);
+            actor.Id = id;
+            context.Entry(actor).State= EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Actores.AnyAsync(x => x.Id == id);
+            if (!existe) { return NotFound(); }
+
+            context.Remove(new Actor() { Id = id });
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
