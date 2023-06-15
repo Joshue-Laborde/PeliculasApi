1b5060cf67c63f32514ac69d32ee1a0aaa49dfb6
Joshue Laborde <joshuelab@gmail.com>
2023-06-15 10:05:54 -0500

    paquete para puntos geograficos

--- PeliculasApi/DTOs/SalasDeCineDTO.cs
+++ PeliculasApi/DTOs/SalasDeCineDTO.cs
@@ -6,5 +6,7 @@ namespace PeliculasApi.DTOs
     {
         public int id { get; set; }
         public string Nombre { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set;}
     }
 }
