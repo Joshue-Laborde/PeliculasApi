a276c1df6bb1daa798d3d622a2399cd1ac801c45
Joshue Laborde <joshuelab@gmail.com>
2023-06-19 17:07:39 -0500

    Pruebas Unitarias - proveedor en memoria de ef core

---
+++ PeliculasApiTest/BasePruebas.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using PeliculasApi;
+using PeliculasApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculasApiTest
+{
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreDB)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreDB).Options;
+
+            var dbContext = new ApplicationDbContext(opciones);
+            return dbContext;
+        }
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                options.AddProfile(new AutoMapperProfiles(geometryFactory));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
